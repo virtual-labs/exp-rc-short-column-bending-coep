a48ccf80f510c3a53a08fdd3235530f0caaf04f2
vleadadmin <admin@vlabs.ac.in>
2025-08-05 09:21:38 +0000

    https://virtual-labs.github.io/exp-rc-short-column-bending-coep click on the link to test your code.

---
+++ simulation/src/columnPage4.js
@@ -0,0 +1,202 @@
+var ascProvided ;
+var buser;
+var duser;
+var acsflg = 0;
+var bflg = 0;
+function page4()
+{		
+	
+		$("#page1Div2").html("");
+		$('#page1Div1').html("");
+		labels1 = '<div class="col-sm-12" id="forceEntered" >'
+		+ '<div class="col-sm-5">'
+		+ '<label  id="enterLoad"  class="" style="font-size:16px;margin:15px 10px ;">Diameter of logitudinal bar  </label>'
+		+ '</div>'
+		+ '<div class="col-sm-3" id="valueStep1">'
+		+ '<input type="text"  value="" id="textD"  style=margin:15px 10px;width:150%;height:50%;" class=" form-control" />'
+		+ '</div>'
+		+ '<div class="col-sm-3"  id="submitStep1">'
+		+ '<br><button type="submit" class="btn btn-danger"  id="dsubmit"  data-target="#myModal" style="width:100%;height:50%;margin-top: -6px;" >Submit</input>'
+		+ '</div>'
+		+ '</div>'
+		
+		
+		+'<div class="col-sm-12" id="forceEntered" >'
+		+ '<div class="col-sm-5">'
+		+ '<label  id="enterLoad"  class="" style="font-size:16px;margin:15px 10px ;">choose number of bars  </label>'
+		+ '</div>'
+		+ '<div class="col-sm-3" id="valueStep1">'
+		+ '<input type="text"  value="" id="txtbar"  style=margin:15px 10px;width:150%;height:50%;" class=" form-control" />'
+		+ '</div>'
+		+ '<div class="col-sm-3"  id="submitStep1">'
+		+ '<br><button type="submit" class="btn btn-danger"  id="bsubmit"  data-target="#myModal" style="width:100%;height:50%;margin-top: -6px;" >Submit</input>'
+		+ '</div>'
+		+ '</div>'
+		
+		$("#page1Div2").html(labels1);
+		
+			$("#dsubmit").click(function() {
+				
+				 duser = $("#textD").val();
+				if(duser < 12){
+					toastr.error('<span class="blinking1">Enter Value greater than 12mm </span>');
+					$("#blink1").prop("hidden",true);
+//					blinker();
+				}else{
+					 $("#dsubmit").prop("disabled",true);
+				}
+				
+		});	
+		
+		$("#bsubmit").click(function() {
+			 buser = $("#txtbar").val();
+			
+			if(buser < 4){
+				toastr.error('<span class="blinking1">Enter Value greater than 4 </span>');
+					$("#blink1").prop("hidden",true);
+				
+			}else{
+				$("#bsubmit").prop("disabled",true);
+				calculateLast();
+				
+			}
+			
+			});
+			
+			function calculateLast(){
+				var acsflg = 0;
+				bflg = 1;
+				columnLast();
+			var ascpro	= (buser * (3.14/4)*duser*duser);
+			ascProvided = parseFloat(ascpro);
+			ascProvided = ascProvided.toFixed(2);
+			if (ascProvided < ASC){
+				acsflg = 1;
+			}
+			
+			var cond2 = duser*16;
+			let cond1 = Math.min(D_val,colSize);
+			let cond3 = 300;
+			let smallest = Math.min(cond1,cond2,cond3);
+//			console.log("Smallest number is:", smallest);
+			
+			label = '<div class = "row" id = "labelrow" >'
+		+'<div class="col-sm-2">'
+		+'</div>'	
+         +'<div class="col-sm-8 marginBottom" id="shearLabel">'
+         +' <center><label class="labelstyle " > ASC provided = '+ascProvided+' mm<sup>2</sup> </label></center> '
+      	+'</div>'
+      	+'</div>'
+      	if(acsflg == 1){
+			  label += '<div class = "row" id = "labelrow" >'
+		+'<div class="col-sm-2">'
+		+'</div>'	
+         +'<div class="col-sm-8 marginBottom" id="shearLabel">'
+         +' <center><label class="labelstyle " > ASC provided should be greater than ASC required </label></center> '
+      	+'</div>'
+      	+'</div>'
+		  }else{
+			  
+		  
+      	label += '<div class = "row" id = "labelrow">'
+      	+ '<div class="col-sm-5">'
+		+ '<label  id="enterLoad"  class="" style="font-size:16px;margin:15px 10px ;">choose diameter of lateral tie()-  </label>'
+		+ '</div>'
+		+ '<div class="col-sm-3" id="valueStep1">'
+		+ '<input type="text"  value="" id="tietxt"  style=margin:15px 10px;width:150%;height:50%;" class=" form-control" />'
+		+ '</div>'
+		+ '<div class="col-sm-3"  id="submitStep1">'
+		+ '<br><button type="submit" class="btn btn-danger"  id="tsubmit" style="width:100%;height:50%;margin-top: -6px;" >Submit</input>'
+		+ '</div>'
+		+'</div>'
+      	
+		
+      	
+      	+'<div class = "row" id = "labelrow">'
+		+'<div class="col-sm-2">'
+		+'</div>'
+      	 +'<div class="col-sm-6 marginBottom" id="shearLabel">'
+         +' <center><label class="labelstyle " >1. Least lateral dimension  = '+cond1+'  </label></center> '
+      	+'</div>'
+      	+'</div>'
+      	
+      	+'<div class = "row" id = "labelrow">'
+		+'<div class="col-sm-2">'
+		+'</div>'
+      	 +'<div class="col-sm-6 marginBottom" id="shearLabel">'
+         +' <center><label class="labelstyle " >2. 16 X Longitudinal bar  = '+cond2+' mm </label></center> '
+      	+'</div>'
+      	+'</div>'
+      	
+      	+'<div class = "row" id = "labelrow">'
+		+'<div class="col-sm-2">'
+		+'</div>'
+      	 +'<div class="col-sm-6 marginBottom" id="shearLabel">'
+         +' <center><label class="labelstyle " > 3. '+cond3+' mm  </label></center> '
+      	+'</div>'
+      	+'</div>'
+      	
+      	+'<div class = "row" id = "labelrow">'
+		+'<div class="col-sm-2">'
+		+'</div>'
+      	 +'<div class="col-sm-6 marginBottom" id="shearLabel">'
+         +' <center><label class="labelstyle " > Lateral spacing of tie bars = '+smallest+' mm  </label></center> '
+      	+'</div>'
+      	+'</div>'
+      	
+
+		
+		
+      	
+     }
+    	
+      	
+      	$("#page1Div2").append(label);
+		
+		
+		function endExperiment() {
+  // Your experiment completion logic here...
+  
+  // Then show the Swal
+//  Swal.fire({
+//    title: 'All Done!',
+//    text: 'Thanks for completing the experiment.',
+//    imageUrl: 'https://media.giphy.com/media/xT9IgzoKnwFNmISR8I/giphy.gif',
+//    imageWidth: 300,
+//    imageHeight: 200,
+//    confirmButtonText: 'Close'
+//  });
+  
+  Swal.fire({
+  icon: 'success',
+  title: 'Experiment Finished!',
+  text: 'Well done. You have completed experiment sucessfully.',
+  confirmButtonText: 'Okay'
+});
+}
+
+					
+			
+			let condtie = Math.max(duser,6);
+			$("#tsubmit").click(function() {
+			tuser = $("#tietxt").val();
+			
+			if(tuser < condtie ){
+				
+					
+					toastr.error('<span class="blinking1">Enter Value greater than '+condtie+' mm </span>');
+					$("#blink1").prop("hidden",true);
+				
+			}else{
+				endExperiment();
+				$("#tsubmit").prop("disabled",true);
+			}
+			
+			});
+			
+			
+		
+			
+		}
+		
+		}
---
+++ theory.md
@@ -0,0 +1,55 @@
+
+### Theory:
+Short columns are RC columns with a slenderness ratio (ratio of effective length to least lateral dimension) less than a specific code limit (typically around 12). Their behaviour is governed by both axial compression and bending moment due to eccentricity of the applied load or inherent imperfections.<br>
+![1](images/image1.png)<br>
+Possible strain profiles under ultimate limit state in eccentriccompression.
+### The design considers:
+
+##### Equilibrium :  
+Balancing the applied axial load (Pu) and bending moment (Mu) with the resisting forces and moments developed by the concrete and steel reinforcement.
+##### Material Properties :
+Characteristic compressive strength of concrete (fck) and characteristic yield strength of steel reinforcement (fy).
+##### Interaction Diagrams :
+These are code-provided charts that relate the axial load capacity (Pu) to the bending moment capacity (Mu) of a specific section for various reinforcement ratios.
+
+### Interaction Curve
+
+The ‘interaction curve’ is a complete graphical representation of the design strength of a uniaxially eccentrically loaded column of given proportions. Each point on the curve corresponds to the design strength values of (P<sub>uR</sub>)  and (M<sub>uR</sub>)  associated with a specific eccentricity (e) of loading. That is to say, if load P is applied on a short column with an eccentricity e, and if this load is gradually increased till the ultimate limit state (defined by the Code) is reached, and that ultimate load at failure is given by Pu = PuR and the corresponding moment by M<sub>u</sub> = M<sub>uR</sub> = P<sub>uR</sub> e, then the coordinates (M<sub>uR</sub>, P<sub>uR</sub>)form a unique point on the interaction diagram.
+The interaction curve defines the different (M<sub>uR</sub>, P<sub>uR</sub>) combinations for all possible eccentricities of loading 0 ≤ e < &infin;. For design purposes, the calculations of M<sub>uR</sub>and P<sub>uR</sub> are based on the design stress-strain curves (including the partial safety factors), and the resulting interaction curve is sometimes referred to as the design interaction curve (which is different from the characteristic interaction curve). Using the design interaction curve for a given column section, it is possible to make a quick judgement as to whether or not the section is ‘safe’ under a specified factored load effect combination (P<sub>u</sub>, M<sub>u</sub>). If the point given by the coordinates (Mu, Pu) falls within the design interaction curve, the column is ‘safe’; otherwise, it is not.<br>
+![2](images/image2.png)<br>
+ Typical Pu -Mu Interaction Diagram
+### Design Steps :
+
+##### Define Design Parameters :
+Specify the applied axial load (P<sub>u</sub>) and bending moment (M<sub>u</sub>) on the column.
+Choose design values for concrete strength (f<sub>ck</sub>) and steel yield strength (f<sub>y</sub>) based on material specifications and code requirements.
+Select a desired concrete cover thickness for durability (refer to building codes).
+
+##### Determine Column Dimensions :
+Considering architectural constraints and spacing requirements, choose a preliminary rectangular or square cross-section for the column (b x h).
+Check code provisions for minimum column dimensions.
+
+##### Effective Length Calculation :
+Based on the end support conditions (fixed, hinged, etc.), calculate the effective length (Le) of the column using code formulas.
+![2](images/image3.png)
+#### Slenderness Ratio Check :
+
+Calculate the slenderness ratio (Le/b) for the chosen dimensions.
+Verify the slenderness ratio is less than the code limit for short column design.
+
+#### Interaction Diagram Selection :
+
+Refer to building codes and select an appropriate interaction diagram for the chosen concrete strength (fck) and reinforcement type. The diagram relates Pu and Mu capacity for various reinforcement ratios.
+
+#### Strength Design :
+
+Assume a trial reinforcement ratio within the allowed limits (typically 0.01 to 0.08).
+Use the interaction diagram to find the corresponding Pu capacity for the chosen reinforcement and section dimensions (b, h).
+If the Pu capacity from the diagram is less than the applied axial load (Pu), adjust the reinforcement ratio and repeat the process until a safe design is achieved (Pu capacity >= Pu and Mu capacity >= Mu).
+#### Serviceability Check :
+Depending on the code and project requirements, perform calculations to ensure the column deflection under service loads (including dead load) is within acceptable limits.
+#### Reinforcement Details :
+Determine the number and diameter of longitudinal steel bars required to achieve the calculated area of reinforcement (Asc).
+Design and specify the spacing, diameter, and minimum requirements for lateral ties to ensure confinement of concrete and prevent buckling of steel bars.
+#### Code Compliance Check :
+Verify the final design meets all relevant code requirements for concrete cover, minimum bar spacing, fire resistance, etc
